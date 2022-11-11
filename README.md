8828177c352a71e6784dc5b6c3a25bc1177cb8ba
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2022-11-10 22:30:09 -0300

    Add TabBar sample - Navigation between pages

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # tab_bar
+
+<div align='center'>
+  <img src='https://github.com/phferreira/assets/blob/master/gifs/tab_bar.gif'/>
+</div>
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TabBar',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 18, 32, 74),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    super.key,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('TabBar'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.home),
+              ),
+              Tab(
+                icon: Icon(Icons.settings),
+              ),
+              Tab(
+                icon: Icon(Icons.person),
+              ),
+            ],
+          ),
+        ),
+        body: Center(
+          child: TabBarView(
+            children: [
+              Container(
+                color: Colors.orange,
+                child: const Icon(Icons.home),
+              ),
+              Container(
+                color: Colors.red,
+                child: const Icon(Icons.settings),
+              ),
+              Container(
+                color: Colors.orange,
+                child: const Icon(Icons.person),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
